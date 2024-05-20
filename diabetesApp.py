b0dab1f00fe9116384a42e6d041b1093a3a20f94
Arijit Guin <rngin7556@gmail.com>
2024-05-20 15:54:09 +0530

    Add files via upload

--- diabetesApp.py
+++ diabetesApp.py
@@ -2,7 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-
+from collections import Counter
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.svm import SVC
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import accuracy_score
 st.write("""
 # Diabetes Prediction App
 
@@ -10,27 +18,106 @@ st.write("""
 
 """)
 
-st.divider()
-st.write("Below are features used to predict the prediction")
-st.write("""<table>
-        <tr><th> Features</th><th> Description</th></tr>
-        <tr><th> Glucose</th><td>Plasma glucose concentration a 2 hours in an oral glucose tolerance test</td></tr>
-        <tr><th> BloodPressure </th><td>Diastolic blood pressure (mm Hg)</td></tr>
-        <tr><th> SkinThickness </th><td>Triceps skin fold thickness (mm)</td></tr>
-        <tr><th> Insulin </th><td>2-Hour serum insulin (mu U/ml)</td></tr>
-        <tr><th> BMI </th><td>Body mass index (weight in kg/(height in m)^2)</td></tr>
-        <tr><th> DiabetesPedigreeFunction </th><td>A higher value implies a stronger diabetes familial link
-            <br><br>If your family members have a low likelihood of diabetes, you may find values in the range of 0.078 to 0.2.
-            <br><br>Values between 0.2 and 0.4 may indicate a moderate likelihood.
-            <br><br>Values beyond 0.4 suggest a relatively higher likelihood of diabetes.</td></tr>
-        <tr><th> Age</th><td>Age in years</td></tr>
-</table><br>""", unsafe_allow_html=True)
 st.write(':blue[_Click the left side bar to insert information_]')
-st.divider()
+
+class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, n_estimators=200, learning_rate=0.1, max_depth=2):
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.models = []
+
+    def fit(self, X, y):
+        # Initialize residuals to y
+        residuals = np.copy(y).astype(float)  # Convert residuals to float
+
+        # Fit base learner
+        for _ in range(self.n_estimators):
+            tree = DecisionTreeRegressor(max_depth=self.max_depth)
+            tree.fit(X, residuals)
+            self.models.append(tree)
+
+            # Update residuals
+            predictions = tree.predict(X)
+            residuals -= self.learning_rate * predictions
+
+    def predict(self, X):
+        predictions = np.zeros(len(X))
+        for tree in self.models:
+            predictions += self.learning_rate * tree.predict(X)
+        return (predictions > 0.5).astype(int)
+
+    def predict_proba(self, X):
+        # Calculate the sum of predictions across all trees
+        sum_predictions = np.zeros(len(X))
+        for tree in self.models:
+            sum_predictions += self.learning_rate * tree.predict(X)
+
+        # Convert to probabilities
+        proba_positive_class = 1 / (1 + np.exp(-sum_predictions))
+        proba_negative_class = 1 - proba_positive_class
+
+        return np.column_stack((proba_negative_class, proba_positive_class))
+
+    def score(self, X, y):
+        return np.mean(self.predict(X) == y)
+
+# Define CustomKNN class
+class CustomKNN(BaseEstimator, ClassifierMixin):
+    def __init__(self, k=5):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        distances = [np.sqrt(np.sum((x - x_train)**2)) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        return Counter(k_nearest_labels).most_common(1)[0][0]
+
+    def predict_proba(self, X):
+        probabilities = []
+        for x in X:
+            distances = [np.sqrt(np.sum((x - x_train)**2)) for x_train in self.X_train]
+            k_indices = np.argsort(distances)[:self.k]
+            k_nearest_labels = [self.y_train[i] for i in k_indices]
+            class_counts = Counter(k_nearest_labels)
+            prob_class_0 = class_counts[0] / self.k
+            prob_class_1 = class_counts[1] / self.k
+            probabilities.append([prob_class_0, prob_class_1])
+        return np.array(probabilities)
+
+class CustomVotingClassifier:
+    def __init__(self, models, weights=None):
+        self.models = models
+        self.weights = weights
+        if weights is None:
+            # initializes with equal weights
+            self.weights = [1/len(models)] * len(models)  
+
+    def fit(self, X, y):
+        for model in self.models:
+            model.fit(X, y)
+        
+        # Calculating weights based on using accuracy meaning:
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+        for i, model in enumerate(self.models):
+            y_pred_val = model.predict(X_val)
+            accuracy = accuracy_score(y_val, y_pred_val)
+            self.weights[i] = accuracy
+
+    def predict(self, X):
+        predictions = np.array([model.predict(X) for model in self.models])
+        weighted_votes = np.average(predictions, axis=0, weights=self.weights)
+        return (weighted_votes > 0.5).astype(int)
 
 st.sidebar.header('Please enter patient details')
-
-from sklearn.impute import SimpleImputer
 
 def calculate_diabetes_pedigree(has_diabetic_relatives):
     if not has_diabetic_relatives:
@@ -60,8 +147,8 @@ def calculate_diabetes_pedigree(has_diabetic_relatives):
             try:
                 relative_type, age_last_exam = relative_info.split(",")
                 age_last_exam = int(age_last_exam)
-                if age_last_exam <= 14:
-                    st.sidebar.warning("'acl' for relatives without diabetes must be greater than 14.")
+                if age_last_exam < 14 or age_last_exam > 88:
+                    st.sidebar.warning("'acl' for relatives without diabetes must be between 14 and 88.")
                     continue
                 relatives_without_diabetes.append((relative_type.strip().lower(), age_last_exam))
             except ValueError:
@@ -69,6 +156,8 @@ def calculate_diabetes_pedigree(has_diabetic_relatives):
 
     numerator_sum = 0
     denominator_sum = 0
+    diabetes_pedigree = (numerator_sum + 20) / (denominator_sum + 50)
+    dpf_value = round(diabetes_pedigree, 3)
 
     for relative_type, age in relatives_with_diabetes:
         shared_genes = get_shared_genes(relative_type)
@@ -78,12 +167,8 @@ def calculate_diabetes_pedigree(has_diabetic_relatives):
         shared_genes = get_shared_genes(relative_type)
         denominator_sum += shared_genes * (age - 14)
 
-    if denominator_sum == 0:
-        dpf_value = 0.078
-        st.sidebar.error("Dividing by zero error: No relatives without diabetes have valid 'acl' values. DPF set to the minimum value (0.078).")
-    else:
-        diabetes_pedigree = (numerator_sum + 20) / (denominator_sum + 50)
-        dpf_value = round(diabetes_pedigree, 3)
+    diabetes_pedigree = (numerator_sum + 20) / (denominator_sum + 50)
+    dpf_value = round(diabetes_pedigree, 3)
 
     st.sidebar.write(f"Diabetes Pedigree Function (DPF) value: {dpf_value}")
     return dpf_value
@@ -103,7 +188,7 @@ def get_shared_genes(relative_type):
     return shared_genes.get(relative_type, 0)
 
 def user_input_features():
-    pregnancies = 0  # Fixed value for pregnancies
+    Pregnancy = st.sidebar.slider('Pregnancies', 0, 17, 3)
     Glucose = st.sidebar.number_input('Glucose', 0, 200, 120)
     BloodPressure = st.sidebar.number_input('BloodPressure (mm Hg)', 0.0, 150.0, 60.0)
     SkinThickness = st.sidebar.number_input('SkinThickness (mm)', 0.1, 100.0, 29.0)
@@ -111,9 +196,11 @@ def user_input_features():
     BMI = st.sidebar.number_input('BMI', 0.0, 70.0, 30.1)
     has_diabetic_relatives = st.sidebar.radio("Do any of your family members have diabetes?", (True, False), index=1)  # Set initial value to False
     DiabetesPedigreeFunction = calculate_diabetes_pedigree(has_diabetic_relatives)
-    Age = st.sidebar.number_input('Age', 0, 120, 30)
+    # DiabetesPedigreeFunction = st.sidebar.number_input('DiabetesPedigreeFunction', 0.0, 3.0, 0.349)
+
+    Age = st.sidebar.number_input('Age', 0, 100, 30)
     data = {
-        'Pregnancies': pregnancies,
+        'Pregnancies': Pregnancy,
         'Glucose': Glucose,
         'BloodPressure': BloodPressure,
         'SkinThickness': SkinThickness,
@@ -129,23 +216,17 @@ def user_input_features():
 with open('standard_scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
-# Function to scale user input features
-def scale_user_input(input_df, scaler):
-    imputer = SimpleImputer(strategy='constant', fill_value=0)
-    input_df_imputed = pd.DataFrame(imputer.fit_transform(input_df), columns=input_df.columns)
-    scaled_input = scaler.transform(input_df_imputed)
-    return scaled_input
-
 input_df = user_input_features()
 
-# Scale the user input features
-scaled_input_df = scale_user_input(input_df, scaler)
+# Preprocess the input data
+input_data = scaler.transform(input_df)
 
 # Reads in saved classification model
-load_clf = pickle.load(open('SVC_KNN.pkl', 'rb'))
+with open('hybrid_model.pkl', 'rb') as f:
+    voting_clf = pickle.load(f)
 
 # Use the model to make predictions
-prediction = load_clf.predict(scaled_input_df)
+prediction = voting_clf.predict(input_data)
 
 # Display the prediction result
 st.subheader('Prediction')
@@ -154,21 +235,53 @@ if prediction[0] == 1:
 else:
     st.success('The patient is not likely to have diabetes.')
 
+st.write("Below are features used to predict the prediction")
+st.write("""
+#### Features and Description
+- **Glucose:** Plasma glucose concentration after a 2-hour oral glucose tolerance test (OGTT) in mg/dl. Reflects how well the body processes glucose.
+  - **Normal Range:**
+    - 2-hour post-OGTT: Less than 140 mg/dL
+  - **Abnormal Range:**
+    - Prediabetes (Impaired Glucose Tolerance): 140-199 mg/dL
+    - Diabetes: 200 mg/dL or higher
+- **Blood Pressure:** Diastolic blood pressure (mm Hg). Measures the pressure in the arteries when the heart rests between beats.
+  - **Normal Range:** Less than 80 mmHg
+  - **Abnormal Range:**
+    - Elevated: 80-89 mmHg
+    - Hypertension Stage 1: 90-99 mmHg
+    - Hypertension Stage 2: 100 mmHg or higher
+- **Skin Thickness:** Triceps skin fold thickness (mm). Indicator of subcutaneous fat.
+  - **Normal Range:** Varies widely depending on age, sex, and body composition.
+- **Insulin:** 2-hour serum insulin (mu U/ml). Reflects the body's insulin response to a glucose challenge.
+  - **Normal Range:** 16-166 mIU/L
+  - **Abnormal Range:**
+    - Low: Below normal range, indicating problems with insulin production.
+    - High: Above normal range, may indicate insulin resistance.
+- **BMI:** Body Mass Index (kg/m2). Measure of body fat based on height and weight.
+  - **Normal Range:** 18.5 - 24.9 kg/m2
+  - **Abnormal Range:**
+    - Underweight: Less than 18.5 kg/m2
+    - Overweight: 25 - 29.9 kg/m2
+    - Obesity Class I: 30 - 34.9 kg/m2
+    - Obesity Class II: 35 - 39.9 kg/m2
+    - Obesity Class III (Severe Obesity): 40 kg/m2 or higher
+- **Diabetes Pedigree Function (DPF):** Estimates the likelihood of developing diabetes based on family history.
+- **Age:** Age in years.
+\n
+These ranges and insights were obtained from the Pima Indians Diabetes Dataset study available at https://github.com/npradaschnor/Pima-Indians-Diabetes-Dataset/tree/master
+         
+#### How to input data for Diabetes Pedigree Function (DPF) estimation:
+- For each relative with diabetes, enter their relationship and age separated by a comma.
+    - Example: parent,50
+- For each relative without diabetes, enter their relationship and age separated by a comma.
+    - Example: sibling,30
 
-# # Calculate metrics
-# accuracy = "83%"
-# precision = "82%"
-# recall = "85%"
-# f1 = "83%"
-# conf_matrix = np.array([[78, 21], [5, 50]])
-
-# # Display metrics
-# st.subheader('Model Performance Metrics')
-# st.write(f'Accuracy: {accuracy}')
-# st.write(f'Precision: {precision}')
-# st.write(f'Recall: {recall}')
-# st.write(f'F1-Score: {f1}')
-
-# # Display confusion matrix
-# st.subheader('Confusion Matrix')
-# st.table(pd.DataFrame(conf_matrix, columns=['Predicted Negative', 'Predicted Positive'], index=['Actual Negative', 'Actual Positive']))
+#### Relatives' Information and Shared Genes:
+- **Parent (e.g., mother or father):** Carries a higher weight (0.5) in determining DPF.
+- **Sibling:** Also carries a higher weight (0.5) similar to parents.
+- **Half-Sibling:** Carries a moderate weight (0.25) in determining DPF.
+- **Grandparent:** Shares genes with you but to a lesser extent (0.25).
+- **Aunt/Uncle:** Shares genes with you but to a lesser extent (0.25).
+- **Half-Aunt/Half-Uncle:** Shares genes with you, but the genetic influence is less (0.125).
+- **Cousin:** Genetic influence is present but minimal (0.125).
+""")
